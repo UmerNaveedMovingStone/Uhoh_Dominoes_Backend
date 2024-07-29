d23be38d89ff422fc549532deae23c99d3e8d788
UmerNaveedMovingStone <umer.naveed@movingstonedigital.com>
2024-07-29 13:28:17 +0500

    Update Authentication.js

--- routes/Authentication.js
+++ routes/Authentication.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const {signup, login, resendVerificationEmail, logoutUser, forgotPassword} = require('../controllers/FirebaseAuthentication');
+const {signup, login, resendVerificationEmail, logoutUser, forgotPassword, deleteuser} = require('../controllers/FirebaseAuthentication');
 
 router.get('/resendEmail', function(req, res, next) {
     var email = req.query.email;
@@ -68,6 +68,21 @@ router.post('/forgot', (req, res)=>{
     var user = req.body
     console.log(req.body)
     forgotPassword(user.email, function () {
+        res.status(200).send({
+            success: true
+        })
+    }, function (error) {
+        res.status(400).send({
+            success: false,
+            message: error.message
+        })
+    })
+})
+
+router.post('/deleteUser', (req, res)=>{
+    var user = req.body
+    console.log(req.body)
+    deleteuser(user.email, function () {
         res.status(200).send({
             success: true
         })
