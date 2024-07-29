449a8d8574d9d8baea858505a49eddf08e78315c
UmerNaveedMovingStone <umer.naveed@movingstonedigital.com>
2024-07-29 14:50:23 +0500

    Update FirebaseAuthentication.js

--- controllers/FirebaseAuthentication.js
+++ controllers/FirebaseAuthentication.js
@@ -229,7 +229,7 @@ function deleteuser(email, successCallback, failureCallback){
         const customToken = await firebaseAdmin.auth().createCustomToken(user.uid);
         await firebase.auth().signInWithCustomToken(customToken);
         const currentUser = firebase.auth().currentUser;
-         firebase.auth().delete(user.uid).then(()=>{
+         firebase.auth().delete_user(user.uid).then(()=>{
             console.log("User Account Deleted")
             successCallback();
         }).catch(err => {
