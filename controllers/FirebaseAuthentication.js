516faedc303455e444f06a30c6a910a839b22557
UmerNaveedMovingStone <umer.naveed@movingstonedigital.com>
2024-07-30 15:45:06 +0500

    Update FirebaseAuthentication.js

--- controllers/FirebaseAuthentication.js
+++ controllers/FirebaseAuthentication.js
@@ -237,8 +237,7 @@ function deleteuser(email, successCallback, failureCallback){
                   if(doc){
                         var data = doc.data();
                           if (data.email === email){
-                                //db.collection('users').doc(doc.id).delete();
-                                data.delete();    
+                                db.collection('users').doc('VNNf6OrlUEKeCqJZ8DZi').delete();
                               //currentUser.delete();
                                 console.log("Doc are not valid" + doc.id);
                                 successCallback();
